6be61611f86772a67d5e52f6e888cf658b090dcc
Serio <SeriousNuts@yandex.ru>
2023-06-08 20:43:29 +0700

    fix run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
 from app import app
-#host = '194.58.123.188'
-host = 'localhost'
-app.run(debug=True, host=host)
+host = '194.58.123.188'
+#host = 'localhost'
+app.run(host=host,port=5000)
